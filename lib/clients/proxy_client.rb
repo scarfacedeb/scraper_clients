16af7ae3def2dd6297cc7e09414b3f5a7a837a2a
Andrew Volozhanin <scarfacedeb@gmail.com>
2017-02-22 16:58:00 +0300

    Add ip_version option to ProxyClient
    Why:
    
    To be able to specify which IPv{4,6} version proxies should be used.

--- lib/clients/proxy_client.rb
+++ lib/clients/proxy_client.rb
@@ -5,17 +5,19 @@ module Clients
   class ProxyClient
     API_URL = "https://proxy6.net/api".freeze
 
-    attr_reader :pool_num
+    attr_reader :pool_num, :ip_version
 
     def initialize(
-      pool_num:,
+      pool_num: 0,
       api_url: API_URL,
-      api_key: ENV["PROXY6_KEY"]
+      api_key: ENV["PROXY6_KEY"],
+      ip_version: "4"
     )
       @pool_num = pool_num
       @api_url = api_url
       @api_key = api_key
       @proxy = fetch_proxy
+      @ip_version = ip_version.to_s
     end
 
     def host
@@ -44,17 +46,24 @@ module Clients
       response = HTTP.accept(:json).get(api_url, params: { state: "active" })
       json = JSON.parse(response.to_s) if response.status.success?
 
-      fail_on_fetch(response) if !response.status.success? || !json.key?("list")
+      fail_on_invalid_list(response) if !response.status.success? || !json.key?("list")
 
       proxies = json.fetch("list")
-      proxies.values.rotate(@pool_num).first
+      fail_on_invalid_list(response) if proxies.is_a?(Array)
+
+      proxies = proxies.values.select { |h| h["version"] == @ip_version }
+      fail_on_invalid_list(response) if proxies.empty?
+
+      proxies
+        .rotate(@pool_num)
+        .first
     end
 
     def api_url
       [@api_url, @api_key, "getproxy/"].join("/")
     end
 
-    def fail_on_fetch(response)
+    def fail_on_invalid_list(response)
       fail ProxyClientError, "Invalid proxy list: #{response.status} #{response.to_s}"
     end
   end
--- lib/clients/version.rb
+++ lib/clients/version.rb
@@ -1,3 +1,3 @@
 module Clients
-  VERSION = "6.2.1"
+  VERSION = "6.3.0"
 end
